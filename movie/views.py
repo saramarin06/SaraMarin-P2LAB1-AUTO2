838d781539deb1b5a9afda71f673f273a90e8617
saramarin06 <saramaring92@gmail.com>
2025-09-04 15:48:08 -0500

    Cambie mi nombre

--- movie/views.py
+++ movie/views.py
@@ -4,8 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html", {'name':'Sara'})
+    return render(request, "home.html", {'name':'Sara Marín'})
 
 def about(request):
-    return HttpResponse("<h1>This is the About Page<h1>")
+    #return HttpResponse("<h1>This is the About Page<h1>")
+    return render(request, "about.html")
     
